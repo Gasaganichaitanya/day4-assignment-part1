0eff24298bc736e50aadb2d9541dab449d8b0960
Gsaganichaitanya <chaitanyagasagani@gmail.com>
2023-11-09 14:35:48 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsApp_part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Chaitu";
+            int age = 22;
+            double height = 5.9;
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(height);
+            Console.ReadKey();
+        }
+    }
+}
